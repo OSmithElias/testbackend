188893f0f7d83836a1757cdec079a93d4e1a69e6
Oneil Smith-Elias <oneilsmithelias@gmail.com>
2023-02-18 16:03:04 -0500

    Checked base endpoint

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express= require('express');
 server= express();
-const port=3000;
+const port=3500;
+const bodyParser = require('body-parser');
+
+//server.use(bodyParser)
 
 server.listen(port, ()=>{
-    console.log(`Listening on port 3000`)
+    console.log(`Listening on port ${port}`)
 })
+
+
+server.get('/', (req,res) => {
+    res.statusCode=200
+    res.send({
+        message:"We did it!!"
+    });
+    console.log(`request recieved`)
+})
+
 
 
